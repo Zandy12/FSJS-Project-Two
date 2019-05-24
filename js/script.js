e87a5df54f56c0d0703ebdbdb57ce6cd0d7b3a8e
Zane Chandy <zanechandy@gmail.com>
2019-05-23 23:06:14 -0400

    Second Commit

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ const page = document.querySelector('div.page');
 const paginationDiv = document.createElement('div');
 const paginationUl = document.createElement('ul');
 paginationUl.className = 'pagination';
+const a = paginationUl.getElementsByTagName('A');
 
 // Global variables for the number of pages and j for iteration 
 let j = 0;
@@ -38,6 +39,9 @@ function appendPageLinks() {
       const a = document.createElement('a');
       a.setAttribute("href", "#" + i);
       a.textContent=i;
+      if (i === 1) {
+         a.className = 'active';
+      }
       li.appendChild(a);
       paginationUl.appendChild(li);
       j = j + numberRestriction;
@@ -49,6 +53,7 @@ function appendPageLinks() {
    }
    paginationDiv.appendChild(paginationUl);
    page.appendChild(paginationDiv);
+
 }
 
 // Function for algorithm that will associate allocated lists per page
@@ -73,29 +78,18 @@ if (studentList.length === 0) {
    appendPageLinks();
 
    // Sets up initial page displaying only the first ten students on the list
-   const a = paginationUl.getElementsByTagName('A');
-   for (let i = 0; i < studentList.length; i++) {
-      if (i > numberRestriction-1) {
-         studentList[i].style.display = 'none';
-      } else {
-         studentList[i].style.display = 'block';
-      }   
-      a[0].style.backgroundColor = '#4ba6c3';
-      a[0].style.color = '#fff';
-   }
+   showPage(studentList, 1);
 
    // Event listener for when page links are pressed 
    paginationUl.addEventListener('click', (e) => {
       for (let i = 0; i < a.length; i++) {
-         a[i].style.backgroundColor = '#fff';
-         a[i].style.color = '#4ba6c3';
+         a[i].className = '';
       }
       for (let i = 0; i <= numberOfPages; i++) {
          if (e.target.textContent === String(i)) {
             showPage(studentList, i);
          }
       }
-      e.target.style.backgroundColor = '#4ba6c3';
-      e.target.style.color = '#fff';
+      e.target.className = 'active';
    });
 }
